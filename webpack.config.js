492595f61ac3e90e8e45433064b9ebde512dc877
Artyom Kayun <kayun.artem@gmail.com>
2016-03-03 08:06:47 +0600

    chore (gulp) add gulp

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const gutil = require('gulp-util');
+const path = require('path');
+
+module.exports = {
+
+  context: path.join(__dirname, 'src/scripts'),
+  entry: {
+    common: './common.js'
+  },
+
+  output: {
+    path: __dirname + '/public/assets/scripts',
+    filename: '[name].js',
+    library: '[name]'
+  },
+
+  resolve: {
+    modulesDirectories: [
+      'node_modules',
+      'src/scripts'
+    ],
+    root: [
+      path.join(__dirname, 'src/scripts'),
+      path.join(__dirname, 'src/templates')
+    ],
+    extensions: ['', '.js', '.jade'],
+    alias: {}
+  },
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: !!gutil.env.dev ? 'inline-source-map' : null,
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jade$/,
+        loader: 'jade'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      minChunks: 2
+    }),
+    new webpack.ProvidePlugin({}),
+    new WebpackNotifierPlugin({
+      title: 'Webpack',
+      alwaysNotify: true
+    })
+  ],
+
+  eslint: {
+    configFile: path.join(__dirname, '.eslintrc'),
+    emitError: true,
+    emitWarning: true
+  }
+};
