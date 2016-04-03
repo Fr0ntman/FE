845bd803170bab7c3c348e1d4e07d425c88eca44
Artyom Kayun <kayun.artem@gmail.com>
2016-04-03 07:12:06 +0600

    feature (common.js) scroll top

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,1 +1,25 @@
+const $ = require('jquery');
+
+$(() => {
+  const $window = $(window);
+  const scrollTop = {
+    duration: 300,
+    $button: $('#js-go-top'),
+    toggleBtn() {
+      if ($window.scrollTop() > 150) {
+        this.$button.addClass('top_state_active');
+      } else {
+        this.$button.removeClass('top_state_active');
+      }
+    },
+
+    scroll() {
+      $('body, html').animate({scrollTop: 0}, this.duration);
+    }
+  };
+
+  $window.scroll($.proxy(scrollTop, 'toggleBtn'));
+  scrollTop.$button.on('click', $.proxy(scrollTop, 'scroll'));
+});
+
 
